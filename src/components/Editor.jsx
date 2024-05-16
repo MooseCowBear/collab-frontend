83df1bf0168c1636c6b59a81e45d5d127bbcc9ca
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-05-16 12:43:32 -0400

    get basic setup working

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,25 @@
-import { useRef } from "react";
+import { defaultKeymap } from "@codemirror/commands";
+import { EditorState } from "@codemirror/state";
+import { basicSetup } from "codemirror";
+import { EditorView, keymap } from "@codemirror/view";
+import { useRef, useEffect } from "react";
 
 export const Editor = () => {
   const editor = useRef();
+
+  useEffect(() => {
+    const startState = EditorState.create({
+      doc: "Hello world",
+      extensions: [basicSetup, keymap.of(defaultKeymap)],
+    });
+
+    const view = new EditorView({ state: startState, parent: editor.current });
+
+    return () => {
+      view.destroy();
+    };
+  }, []);
+
   return <div ref={editor}></div>;
 };
+
